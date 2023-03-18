6f6fe11326bd8964331890f989886a931549326c
Inga <inga.gudaite@gmail.com>
2023-03-18 10:09:33 +0200

    Home page

---
+++ r2/src/Components/HomePage.jsx
@@ -0,0 +1,14 @@
+function HomePage() {
+    
+    return (
+        <>
+            <div class="container">
+                <div class="square"></div>
+                <div class="square"></div>
+                <h1 class="title">Welcome to my website</h1>
+            </div>
+        </>
+    );
+}
+
+export default HomePage;
---
+++ r2/src/HompePage.jsx
@@ -0,0 +1,12 @@
+import HomePage from './Components/HomePage';
+import './style/app.scss';
+
+
+function App() {
+
+    return (
+        <HomePage />
+    )
+}
+
+export default App;
