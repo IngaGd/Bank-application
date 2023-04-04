d8be71ef43cafd4f731243d00f91802e5118a8b7
Inga <inga.gudaite@gmail.com>
2023-04-04 14:42:45 +0300

    refactoring

--- bank/src/Components/Sort.jsx
+++ bank/src/Components/Sort.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Sort({ setSort }) {
     return (
         <div className='sort-container'>
-            <button className="sort-btn" onClick={() => setSort('name')}>Sort by Name</button>
+            <button className="sort-btn" onClick={() => setSort('name')}>Sort by Surame</button>
             <button className="sort-btn" onClick={() => setSort('balance')}>Sort by Balance</button>
             {/* <button className="sort-btn" onClick={() => setSort('unsort')}>Unsort</button> */}
         </div>
