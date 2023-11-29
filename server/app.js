aaf47142758b2c881a9d70db0c09bbc9c5ee7abe
Inga <inga.gudaite@gmail.com>
2023-11-29 14:24:03 +0200

    refactoring

--- server/app.js
+++ server/app.js
@@ -14,12 +14,13 @@ app.use(express.json({ limit: '10mb' })); //pasididinom uploadinamo failo dydzio
 app.use(express.static('public')); //nurodom, kur laikom statinius failus
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'bankApplication'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
 })
-
+console.log('Connecting to the database with host:', process.env.DB_HOST);
 //pasinaudojam, sutikimas, kad serveris gautu js is narsykles
 app.use(
     cors({
@@ -326,113 +327,6 @@ app.get('/login', (req, res) => {
         }
     });
 });
-
-
-//LOGIN
-// app.post('/login', (req, res) => {
-//     const users = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'));
-
-//     const name = req.body.userName;
-//     const password = md5(req.body.userPsw);
-
-//     console.log(password);
-
-//     const user = users.find(u => u.name === name && u.password === password);
-
-//     //jei user yra, generuojam sesijos id
-//     if (user) {
-//         const sessionId = md5(uuidv4());
-//         user.session = sessionId; //priskiriam sesijos id
-//         fs.writeFileSync('./data/users.json', JSON.stringify(users), 'utf8')    //isaugom visus userius, vienas bus su spec tokenu
-//         //kai useris turi sesijos id, issiunciam cookie
-//         res.cookie('cookieSession', sessionId);
-//         res.json({
-//             status: 'valid',
-//             getName: user.name
-//         });
-//         res.redirect('/home');
-//     } else {
-//         res.json({
-//             status: 'error',
-//         });
-//     }
-// })
-
-
-// app.get('/login', (req, res) => {
-//     const users = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'));
-//     const user = req.cookies.cookieSession ?
-//         users.find(u => u.session === req.cookies.cookieSession) :
-//         null;
-//     if (user) {
-//         res.json({
-//             status: 'valid',
-//             getName: user.name,
-//             role: user.role
-//         });
-//     } else {
-//         res.json({
-//             status: 'error'
-//         })
-//     }
-// })
-
-// //LOGOUT
-
-// app.post('/logout', (req, res) => {
-//     res.cookie('cookieSession', '***');
-//     res.json({
-//         status: 'logout',
-//     });
-// });
-
-// // USERS
-
-// app.get('/users', (req, res) => {
-//     // jei ateinam su get metodu, paverciam duomenis i json ir issiunciam i serveri
-//     let allData = fs.readFileSync('./data/users.json', 'utf8'); //nuskaitom duomenis is failo (stringa)
-//     allData = JSON.parse(allData);
-//     res.json(allData); // issiunciam i serveri duomenis, kuiuos gavome nuskaite is failo
-// });
-
-// app.post('/register', (req, res) => {
-//     let allData = fs.readFileSync('./data/users.json', 'utf8');
-//     allData = JSON.parse(allData);
-//     const id = uuidv4();
-//     const data = {
-//         role: 'manager',
-//         name: req.body.userName,
-//         password: md5(req.body.userPsw),
-//         id
-//     };
-//     allData.push(data);
-//     allData = JSON.stringify(allData);
-//     fs.writeFileSync('./data/users.json', allData, 'utf8');
-//     res.json({
-//         status: 'valid'
-//     });
-// });
-
-
-// app.delete('/users/:id', (req, res) => {
-//     //kintamus parametrus rasom :...
-//     //is requesto paimam params, kurio vardas yra id
-//     //pradzia kaip ir creato
-//     let allData = fs.readFileSync('./data/users.json', 'utf8'); //nuskaitom duomenis is failo (stringa)
-//     allData = JSON.parse(allData);
-//     const userToDelete = allData.find(d => req.params.id === d.id);
-//     if (userToDelete.role === 'admin') {
-//         res.status(400).json({});
-//     } else {
-//         let deletedData = allData.filter((d) => req.params.id !== d.id);
-//         //deleted data sustringifyinta
-//         deletedData = JSON.stringify(deletedData);
-//         fs.writeFileSync('./data/users.json', deletedData, 'utf8');
-//         res.json({
-//             message: { text: 'User was deleted successfully', type: 'positive' },
-//         });
-//     }
-// });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
