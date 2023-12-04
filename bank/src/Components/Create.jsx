321ea1d6e91536818d0c077046760a097cae5b31
Inga <inga.gudaite@gmail.com>
2023-12-04 17:19:42 +0200

    component 'create' styling

--- bank/src/Components/Create.jsx
+++ bank/src/Components/Create.jsx
@@ -27,59 +27,53 @@ function Create() {
     return (
         <div className="create">
             <div className="title">Create account</div>
-            <div className="row">
-                <div className="content">
-                    <div className="file-upload">
-                        <div className="file-upload__container">
-                            <div className="image-box">
-                                {file ? (
-                                    <img
-                                        className="image"
-                                        src={file}
-                                        alt="upload"
-                                    />
-                                ) : null}
-                            </div>
-                        </div>
-
-                        <div className="file-upload__input-group">
-                            <label className="label">
-                                Choose image (.png){' '}
-                            </label>
-                            <input
-                                className="input"
-                                type="file"
-                                onChange={readFile}
-                            />
+            <div className="content">
+                <div className="file-upload">
+                    <div className="file-upload__container">
+                        <div className="image-box">
+                            {file ? (
+                                <img
+                                    className="image"
+                                    src={file}
+                                    alt="upload"
+                                />
+                            ) : null}
                         </div>
                     </div>
-                    <div className="account-details">
-                        <div className="input-group">
-                            <div className="input-box">
-                                <label className="label">Set name</label>
-                                <input
-                                    className="input"
-                                    type="text"
-                                    value={name}
-                                    onChange={(e) => setName(e.target.value)}
-                                />
-                            </div>
 
-                            <div className="input-box">
-                                <label className="label">Set surname</label>
-                                <input
-                                    className="input"
-                                    type="text"
-                                    value={surname}
-                                    onChange={(e) => setSurname(e.target.value)}
-                                />
-                            </div>
-                        </div>
+                    <div className="file-upload__input-group">
+                        <label className="label">Choose image (.png) </label>
+                        <input
+                            className="input"
+                            type="file"
+                            onChange={readFile}
+                        />
                     </div>
-                    <button className="btn--create" onClick={create}>
-                        Create account
-                    </button>
                 </div>
+                <div className="account-details">
+                    <div className="input-box">
+                        <label className="label">Set name</label>
+                        <input
+                            className="input"
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="input-box">
+                        <label className="label">Set surname</label>
+                        <input
+                            className="input"
+                            type="text"
+                            value={surname}
+                            onChange={(e) => setSurname(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <button className="btn--create" onClick={create}>
+                    Create account
+                </button>
             </div>
         </div>
     );
