45cbeca3529a7147d714d4d3078b7901a3176a90
Inga <inga.gudaite@gmail.com>
2023-12-05 17:28:36 +0200

    Totals component layout

--- bank/src/Components/Totals.jsx
+++ bank/src/Components/Totals.jsx
@@ -14,24 +14,26 @@ function Totals() {
 
     return (
         <div className="totals">
-            Total balances: <span>{totalBalances}</span>
-            <br />
-            Number of accounts: <span>{numAccounts}</span>
-            <br />
-            Number of accounts with uploaded images:{' '}
-            <span>{numWithImages}</span>
-            <br />
-            Number of accounts with default image:{' '}
-            <span>{numWithDefaultImage}</span>
-            <br />
-            Number of accounts with zero balance:{' '}
-            <span>{numWithZeroBalance}</span> <br />
-            Number of accounts with positive balance:{' '}
-            <span>{numWithPositiveBalance}</span>
-            <br />
-            Number of accounts with negative balance:{' '}
-            <span>{numWithNegativeBalance}</span>
-            <br />
+            <div className="totals__content">
+                Total balances: <span>{totalBalances}</span>
+                <br />
+                Number of accounts: <span>{numAccounts}</span>
+                <br />
+                Number of accounts with uploaded images:{' '}
+                <span>{numWithImages}</span>
+                <br />
+                Number of accounts with default image:{' '}
+                <span>{numWithDefaultImage}</span>
+                <br />
+                Number of accounts with zero balance:{' '}
+                <span>{numWithZeroBalance}</span> <br />
+                Number of accounts with positive balance:{' '}
+                <span>{numWithPositiveBalance}</span>
+                <br />
+                Number of accounts with negative balance:{' '}
+                <span>{numWithNegativeBalance}</span>
+                <br />
+            </div>
         </div>
     );
 }
