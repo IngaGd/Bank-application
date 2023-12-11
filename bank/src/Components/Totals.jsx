4333367b7e0a5019249aeaf81e2d6130e5a0f498
Inga <inga.gudaite@gmail.com>
2023-12-11 14:26:26 +0200

    Chart for totals

--- bank/src/Components/Totals.jsx
+++ bank/src/Components/Totals.jsx
@@ -1,5 +1,14 @@
 import { useContext } from 'react';
 import { GlobalContext } from './GlobalContext';
+import { Chart as ChartJS, defaults } from 'chart.js/auto';
+import { Doughnut } from 'react-chartjs-2';
+
+//defaults.maintainAspectRatio = false;
+defaults.responsive = true;
+defaults.backgroundColor = '#1f1f1f';
+defaults.borderColor = '#1f1f1f';
+defaults.color = '#e9e9e9';
+defaults.scaleLineColor = 'rgba(0,0,0,0)';
 
 function Totals() {
     const {
@@ -12,9 +21,74 @@ function Totals() {
         numWithNegativeBalance,
     } = useContext(GlobalContext);
 
+    const chartData = [
+        {
+            label: 'Total accounts',
+            value: numAccounts,
+        },
+        {
+            label: 'Accounts with images',
+            value: numWithImages,
+        },
+        {
+            label: 'Accounts without image',
+            value: numWithDefaultImage,
+        },
+        {
+            label: 'Accounts with 0 balance',
+            value: numWithZeroBalance,
+        },
+        {
+            label: 'Accounts with positive balance',
+            value: numWithPositiveBalance,
+        },
+        {
+            label: 'Accounts with negative balance',
+            value: numWithNegativeBalance,
+        },
+    ];
+
+    console.log(
+        'Chart data values:',
+        chartData.map((data) => data.value)
+    );
+
     return (
         <div className="totals">
-            <div className="totals__content">
+            <div className="totals__chart">
+                <Doughnut
+                    data={{
+                        labels: chartData.map((data) => data.label),
+                        datasets: [
+                            {
+                                label: 'Count',
+                                data: chartData.map((data) => data.value),
+                                backgroundColor: [
+                                    'yellow',
+                                    'green',
+                                    'crimson',
+                                    'blue',
+                                    'pink',
+                                    'orange',
+                                    'violet',
+                                ],
+                                borderColor: 'rgb(31, 31, 31)',
+                            },
+                        ],
+                        // options={{
+                        //     layout: {
+                        //         padding: 20,
+                        //     },
+                        // }}
+                    }}
+                    // options={{
+                    //     layout: {
+                    //         padding: 20,
+                    //     },
+                    // }}
+                />
+            </div>
+            {/* <div className="totals__content">
                 Total balances: <span>{totalBalances}</span>
                 <br />
                 Number of accounts: <span>{numAccounts}</span>
@@ -33,7 +107,7 @@ function Totals() {
                 Number of accounts with negative balance:{' '}
                 <span>{numWithNegativeBalance}</span>
                 <br />
-            </div>
+            </div> */}
         </div>
     );
 }
