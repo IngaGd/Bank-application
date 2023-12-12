8409bb3e9983ae665e656c021a228b745e439571
Inga <inga.gudaite@gmail.com>
2023-12-12 19:28:07 +0200

    Edit modal responsive

--- bank/src/Components/Info.jsx
+++ bank/src/Components/Info.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 export default function Info() {
     return (
         <div className="info">
-            <h1 className="info__main-title">Your accounts</h1>
+            <h1 className="info__main-title">Bank accounts</h1>
         </div>
     );
 }
